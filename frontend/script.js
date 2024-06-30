853d2088e52084a5e2cf545abf55c0a2bdc30c17
Artem Fediai <artem.fediai@nanomatch.com>
2024-06-30 18:07:24 +0200

    Updated API Gateway URL and added .gitignore

--- frontend/script.js
+++ frontend/script.js
@@ -2,14 +2,14 @@ document.getElementById('computeForm').addEventListener('submit', async function
     event.preventDefault();
     const a = parseFloat(document.getElementById('a').value);
     const b = parseFloat(document.getElementById('b').value);
-    const response = await fetch('YOUR_API_GATEWAY_URL/compute', {
+    const response = await fetch('https://ukkjifvu2b.execute-api.eu-central-1.amazonaws.com/Prod/compute', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'x-api-key': 'YOUR_API_KEY' // Replace with your actual API key if authentication is enabled
         },
         body: JSON.stringify({ a, b })
     });
     const data = await response.json();
     document.getElementById('result').innerText = `Result: ${data.c}`;
 });
-
